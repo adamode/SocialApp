2d83deca53bb0720ec296a5e531d04268ed46266
adamode <mode388@gmail.com>
2017-03-11 16:12:54 +0800

    Sign In UI Custom Class Complete

---
+++ SocialApp/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SocialApp
+//
+//  Created by Mohd Adam on 11/03/2017.
+//  Copyright © 2017 Mohd Adam. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
---
+++ SocialApp/FancyField.swift
@@ -0,0 +1,36 @@
+//
+//  FancyField.swift
+//  SocialApp
+//
+//  Created by Mohd Adam on 11/03/2017.
+//  Copyright © 2017 Mohd Adam. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+
+    override func awakeFromNib() {
+        
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        
+        layer.borderWidth = 1.0
+        
+        layer.cornerRadius = 2.0
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+
+}
+    
